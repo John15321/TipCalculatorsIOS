8cc76d0e6483e380b4ba21409e230ba27b2a0f98
Jan Bronicki <janbronicki@gmail.com>
2021-05-10 23:20:58 +0200

    Initial Commit

---
+++ TipCalculatorsIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculatorsIOS
+//
+//  Created by Janek on 10/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
