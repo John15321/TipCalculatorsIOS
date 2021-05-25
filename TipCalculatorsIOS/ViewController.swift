d8c16271521d2ce8d0d5b01bac9d52574bb2d88c
Jan Bronicki <56229312+John15321@users.noreply.github.com>
2021-05-25 12:57:43 +0200

    .

--- TipCalculatorsIOS/ViewController.swift
+++ TipCalculatorsIOS/ViewController.swift
@@ -15,17 +15,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipText: UITextField!
     @IBOutlet weak var billText: UITextField!
     @IBAction func calculateTip(_ sender: Any) {
-        let tipPercent = Double(tipText.text!)
-        let billAmount = Double(billText.text!)
-//        let reviewStars = Int(reviewText.text!)
-        let totalBill = Double(tipPercent!*billAmount!/100 + billAmount!)
-//        if totalBill
-//        {
+        if let tipPercent = Double(tipText.text!),
+           let billAmount = Double(billText.text!) {
+        let totalBill = Double(tipPercent*billAmount/100 + billAmount)
         outputLabel.text = String(totalBill)
-//        }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+                
+        view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
     }
 }
